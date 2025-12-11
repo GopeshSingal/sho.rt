e6396e46cf9eba5e3e34d0582a0aa46852a99ccb
GopeshSingal <gopesh.singal.03@gmail.com>
2025-12-11 05:45:04 +0000

    Implement Azure Static Web App module to serve as front-facing site for user interaction

---
+++ infra/modules/static_web_app/main.tf
@@ -0,0 +1,10 @@
+resource "azurerm_static_web_app" "site" {
+  name                = var.name
+  resource_group_name = var.resource_group_name
+  location            = var.location
+
+  sku_tier = "Free"
+  sku_size = "Free"
+
+  tags = var.tags
+}
